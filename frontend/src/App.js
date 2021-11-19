8690c0a3576f1611044accd4f9d7d25adfe11eb7
Az-s <azim.sa@yahoo.com>
2021-11-19 13:53:25 +0600

    added all routes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import { CssBaseline } from '@mui/material';
+import Posts from './containers/Posts/Posts';
+import NewPost from './containers/NewPost/NewPost';
+import Post from './containers/Post/Post';
+import Login from './containers/Login/Login';
+import Register from './containers/Register/Register';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <Router>
+        <NavBar />
+        <Route exact path="/" component={Posts} />
+        <Route path="/posts/new" component={NewPost} />
+        <Route path="/posts/:id" component={Post} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Router>
     </div>
   );
 }

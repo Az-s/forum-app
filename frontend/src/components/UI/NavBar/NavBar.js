88e168004d69e53af1cd0fabcae83bc747aaa0d9
Az-s <azim.sa@yahoo.com>
2021-11-19 14:00:17 +0600

    created config and axiosApi

--- frontend/src/components/UI/NavBar/NavBar.js
+++ frontend/src/components/UI/NavBar/NavBar.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { AppBar, Box, Toolbar, Typography, Button, Grid } from '@mui/material';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import UserMenu from './Menu/UserMenu';
+import AnonymousMenu from './Menu/AnonymousMenu';
 
 const NavBar = () => {
+    const user = useSelector(state => state.users.user);
+
     return (
-        <div>
-            
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar variant="dense" sx={{ justifyContent: 'center' }}>
+                    <Typography variant="h6" component={Link} to="/" sx={{ color: 'white', textDecoration: 'none', flexGrow: 1 }}>
+                        Forum
+                    </Typography>
+                    <Grid item>
+                        {user ? (
+                            <UserMenu user={user} />
+                        ) : (
+                            <AnonymousMenu />
+                        )}
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
